2b1f43fe8a8d6565ff532c088e47a69e3f0e1f94
vonabe <vonabe.dev@gmail.com>
2018-04-05 13:18:33 +0300

    Kirill petuch

--- src/customhotkeysapp/CustomHotkeys.java
+++ src/customhotkeysapp/CustomHotkeys.java
@@ -29,27 +29,27 @@ public class CustomHotkeys extends Application {
 
     private static TrayIcon trayIcon = null;
     final public static String version = "v0.0.3";
-    final public static String name_version = "CustomHotkeys "+version;
+    final public static String name_version = "CustomHotkeys " + version;
     private MenuItem item0 = null, item1 = null;
 
     public static String PATH_FILE = CustomHotkeys.class.getProtectionDomain().getCodeSource().getLocation().getFile().substring(1).replace("/", "\\");
-    public static String PATH = (System.getProperty("user.dir")+"\\").replace("\\", "/");
+    public static String PATH = (System.getProperty("user.dir") + "\\").replace("\\", "/");
     public static String java_home = System.getProperty("java.home");
 
     @Override
     public void start(final Stage stage) throws Exception {
-    	Platform.setImplicitExit(false);
-    	System.out.println(PATH);
+        Platform.setImplicitExit(false);
+        System.out.println(PATH);
 
-    	List<String> args = getParameters().getRaw();
-    	boolean hide = false;
-    	if(args.size() > 0){
-    		String info0 = args.get(0);
+        List<String> args = getParameters().getRaw();
+        boolean hide = false;
+        if (args.size() > 0) {
+            String info0 = args.get(0);
 
-    		if(info0.equals("/hide")){
-    			hide = true;
-    		}
-    	}
+            if (info0.equals("/hide")) {
+                hide = true;
+            }
+        }
 
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
 
@@ -58,91 +58,91 @@ public class CustomHotkeys extends Application {
         stage.getIcons().add(new Image("res/ic_launcher.png", false));
         stage.setScene(scene);
 
-        if(hide)
-        	stage.hide();
-        else
-        	stage.show();
+        if (hide) {
+            stage.hide();
+        } else {
+            stage.show();
+        }
 
         stage.setOnCloseRequest((WindowEvent event) -> {
-        	FXMLDocumentController.saveAllTab();
+            FXMLDocumentController.saveAllTab();
             System.exit(0);
         });
 
-        try{
-	        if(SystemTray.isSupported()){
+        try {
+            if (SystemTray.isSupported()) {
 
-	        	 String autorun = null;
-	             if(Autorun.isRegistration()){
-	             	autorun = "off";
-	             }else{
-	             	autorun = "on";
-	             }
+                String autorun = null;
+                if (Autorun.isRegistration()) {
+                    autorun = "off";
+                } else {
+                    autorun = "on";
+                }
 
-		        SystemTray tray = SystemTray.getSystemTray();
-		        PopupMenu popup = new PopupMenu();
-		        java.awt.Image imgTray = SwingFXUtils.fromFXImage(new javafx.scene.image.Image("res/ic_launcher.png"), null);
-		        int trayIconWidth = tray.getTrayIconSize().width;
+                SystemTray tray = SystemTray.getSystemTray();
+                PopupMenu popup = new PopupMenu();
+                java.awt.Image imgTray = SwingFXUtils.fromFXImage(new javafx.scene.image.Image("res/ic_launcher.png"), null);
+                int trayIconWidth = tray.getTrayIconSize().width;
 
-		        trayIcon = new TrayIcon(imgTray.getScaledInstance(trayIconWidth, -1, java.awt.Image.SCALE_SMOOTH), name_version, popup);
-		        trayIcon.setImageAutoSize(true);
-		        tray.add(trayIcon);
+                trayIcon = new TrayIcon(imgTray.getScaledInstance(trayIconWidth, -1, java.awt.Image.SCALE_SMOOTH), name_version, popup);
+                trayIcon.setImageAutoSize(true);
+                tray.add(trayIcon);
 
-		        this.item0 = new MenuItem("Autorun "+autorun);
-		        this.item0.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						boolean reg = Autorun.isRegistration();
-						editRegAutorun(!reg);
-						editParamReg(!reg);
-					}
-				});
-		        popup.add(this.item0);
+                this.item0 = new MenuItem("Autorun " + autorun);
+                this.item0.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        boolean reg = Autorun.isRegistration();
+                        editRegAutorun(!reg);
+                        editParamReg(!reg);
+                    }
+                });
+                popup.add(this.item0);
 
-		        this.item1 = new MenuItem("Exit");
-		        this.item1.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						FXMLDocumentController.saveAllTab();
-			            System.exit(0);
-					}
-				});
-		        popup.add(this.item1);
+                this.item1 = new MenuItem("Exit");
+                this.item1.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        FXMLDocumentController.saveAllTab();
+                        System.exit(0);
+                    }
+                });
+                popup.add(this.item1);
 
-		        stage.iconifiedProperty().addListener(new ChangeListener<Boolean>() {
-		        	@Override
-		        	public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-		        		Platform.runLater(new Runnable() {
-							@Override
-							public void run() {
-								if(newValue){
-					        		stage.hide();
-								}
-							}
-		        		});
-		        	}
-				});
+                stage.iconifiedProperty().addListener(new ChangeListener<Boolean>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (newValue) {
+                                    stage.hide();
+                                }
+                            }
+                        });
+                    }
+                });
 
-		        trayIcon.addActionListener(new ActionListener() {
-		   			@Override
-		   			public void actionPerformed(ActionEvent e) {
-		   				Platform.runLater(new Runnable() {
-							@Override
-							public void run() {
-				   				stage.show();
-				   				stage.setIconified(false);
-							}
-						});
-		   			}
-		   		});
-	        }
-	    	new UpdateVersion().GET_UPDATE();
+                trayIcon.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                stage.show();
+                                stage.setIconified(false);
+                            }
+                        });
+                    }
+                });
+            }
+            new UpdateVersion().GET_UPDATE();
 
-    }catch (Exception e) {
-    	System.out.println("exception SystemTray or WinRegistry"+e.getMessage());
-	}
+        } catch (Exception e) {
+            System.out.println("exception SystemTray or WinRegistry" + e.getMessage());
+        }
 
     }
-
 
     private void editParamReg(boolean parambool) {
         if (parambool) {
@@ -155,21 +155,22 @@ public class CustomHotkeys extends Application {
     private void editRegAutorun(boolean autorun) {
         if (autorun) {
             boolean reg = Autorun.registration();
-            this.item0.setLabel("Autorun "+((reg)?"off":"on"));
+            this.item0.setLabel("Autorun " + ((reg) ? "off" : "on"));
         } else {
             boolean unreg = Autorun.unregistration();
-            this.item0.setLabel("Autorun "+((!unreg)?"off":"on"));
+            this.item0.setLabel("Autorun " + ((!unreg) ? "off" : "on"));
         }
     }
 
-    public static void message(Object title, Object message, TrayIcon.MessageType type){
+    public static void message(Object title, Object message, TrayIcon.MessageType type) {
 //        try {
-            if(trayIcon!=null)trayIcon.displayMessage(title.toString(), message.toString(), type);
+        if (trayIcon != null) {
+            trayIcon.displayMessage(title.toString(), message.toString(), type);
+        }
 //        } catch (UnsupportedEncodingException ex) {
 //            System.err.println("error encoding string");
 //        }
     }
-
 
     @Override
     public void init() throws Exception {
@@ -181,7 +182,7 @@ public class CustomHotkeys extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	launch(args);
+        launch(args);
     }
 
 }
